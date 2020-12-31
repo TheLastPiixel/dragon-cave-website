b512e4f7fd72e697f6f836554099f2d3d2814c5d
TheLastPiixel <48301981+TheLastPiixel@users.noreply.github.com>
2020-12-31 21:40:59 +1300

    Added basic contact us form (Still needs styling & scrip)

--- src/Pages/Contact/Contact.js
+++ src/Pages/Contact/Contact.js
@@ -1,5 +1,6 @@
 import "./Contact.css";
 import React, { Component } from 'react';
+import {Form, InputGroup, Button} from 'react-bootstrap';
 import Map from "./ContactComponents/Map";
 import HL from '../../Components/HL';
 
@@ -9,20 +10,42 @@ class Contact extends Component {
       <div class="row">
         <div class="col-sm-8" style={{margin: "0 auto"}}>
 
-          <div class="col-sm-8 float-left" style={{height: "50vh", marginTop: "20px"}}>
+          <div class="col-sm-8 float-left" style={{marginTop: "20px"}}>
             <Map />
           </div>
 
           <div class="col-sm-4 float-right" style={{padding: "20px"}}>
-            <h1 class="Text">Contact</h1>
             <p class="Text"><b>Address:</b> L-3-15, Jaya shopping mall, Jalan Professor Khoo Kay Kim, Section 14, 46100 Petaling Jaya, Selangor, Malaysia</p>
-            <p class="Text"><b>Email:</b> dragoncave@gmail.com</p>
-            <p class="Text"><b>Phone:</b> +60379321523</p>
-            <p class="Text"><b>Mobile:</b> +60379321523</p>
+            <p class="Text"><b>Email:</b> <a href="mailto:dragoncave@gmail.com">dragoncave@gmail.com</a></p>
+            <p class="Text"><b>Phone:</b> <a href="tel:+60379321523">+60379321523</a></p>
+            <p class="Text"><b>Mobile:</b> <a href="tel:+60379321523">+60379321523</a></p>
           </div>
-
         </div>
         <HL />
+
+        <div class="col-sm-5" style={{margin: "0 auto"}}>
+          <h1>Contact Us</h1>
+          <Form>
+            <Form.Group controlId="Name">
+              <Form.Control type="text" placeholder="Name" />
+            </Form.Group>
+            <Form.Group controlId="Email">
+              <Form.Control type="email" placeholder="Email" />
+            </Form.Group>
+            <Form.Group controlId="Phone">
+              <Form.Control type="tel" placeholder="Phone" />
+            </Form.Group>
+            <Form.Group controlId="Subject">
+              <Form.Control type="text" placeholder="Subject" />
+            </Form.Group>
+            <Form.Group controlId="Message">
+              <Form.Control as="textarea" rows={5} type="text" placeholder="Message"/>
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+          </Form>
+        </div>
       </div>
     );
   }

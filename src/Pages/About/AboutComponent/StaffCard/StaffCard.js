bb264d2e25d1f83814664c6b05b7c1d0894fd7c2
TheLastPiixel <48301981+TheLastPiixel@users.noreply.github.com>
2021-01-18 23:50:08 +1300

    Minor changes to staff profiles

--- src/Pages/About/AboutComponent/StaffCard/StaffCard.js
+++ src/Pages/About/AboutComponent/StaffCard/StaffCard.js
@@ -2,18 +2,19 @@ import './StaffCard.css';
 import React, { useState } from 'react';
 import {Card} from 'react-bootstrap';
 import PlateSet from '../../../../Content/Image/plate-set.jpg';
+import Photo from './test.jpg';
 
 function StaffCard() {
     const [isShown, setIsShown] = useState(false);
 
     return (
       <Card 
-      class="card bg-dark text-white Card" 
+      class="card bg-dark text-white CardDiv" 
       style={{ width: '18rem'}}  
       onMouseEnter={() => setIsShown(true)} 
       onMouseLeave={() => setIsShown(false)}
       >
-        <Card.Img variant="top" src={PlateSet} alt="Item Image" />
+        <Card.Img variant="top" src={Photo} alt="Item Image" />
           {isShown && (
             <div class="card-img-overlay">
               <p class="Text">
@@ -21,6 +22,10 @@ function StaffCard() {
               </p>
             </div>
           )}
+          <div class="Text card-img-overlay Details">
+            <h4>Joseph Tze Ming Chua</h4>
+            <h7>Staff Member</h7>
+          </div>
       </Card>
     );
   }
